25258f0018d4dd8f4796b6621a96f68d0d80e6dc
kajal1801 <anshikajais2001@gmail.com>
2023-03-10 20:48:15 +0530

    Updated

--- index.js
+++ index.js
@@ -6,47 +6,47 @@ let sumGuest = 0
 localStorage.setItem("sumHome", sumHome)
 localStorage.setItem("sumGuest", sumGuest)
 
-function addOneHome(){
+document.getElementById('addOneHome').addEventListener('click', function() {
     sumHome++
     localStorage.setItem("sumHome", sumHome)
     scoreHome.innerHTML = localStorage.getItem("sumHome")
-}
+})
 
-function addTwoHome(){
+document.getElementById('addTwoHome').addEventListener('click', function(){
     sumHome += 2
     localStorage.setItem("sumHome", sumHome)
     scoreHome.innerHTML = localStorage.getItem("sumHome")
-}
+})
 
-function addThreeHome(){
+document.getElementById('addThreeHome').addEventListener('click', function(){
     sumHome += 3
     localStorage.setItem("sumHome", sumHome)
     scoreHome.innerHTML = localStorage.getItem("sumHome")
-}
+})
 
-function addOneGuest(){
+document.getElementById('addOneGuest').addEventListener('click', function(){
     sumGuest++
     localStorage.setItem("sumGuest", sumGuest)
     scoreGuest.innerHTML = localStorage.getItem("sumGuest")
-}
+})
 
-function addTwoGuest(){
+document.getElementById('addTwoGuest').addEventListener('click', function(){
     sumGuest += 2
     localStorage.setItem("sumGuest", sumGuest)
     scoreGuest.innerHTML = localStorage.getItem("sumGuest")
-}
+})
 
-function addThreeGuest(){
+document.getElementById('addThreeGuest').addEventListener('click', function(){
     sumGuest += 3
     localStorage.setItem("sumGuest", sumGuest)
     scoreGuest.innerHTML = localStorage.getItem("sumGuest")
-}
+})
 
-function newGame(){
+document.getElementById('new-game').addEventListener('click', function(){
     sumHome = 0
     sumGuest = 0
     localStorage.setItem("sumGuest", 0)
     localStorage.setItem("sumHome", 0)
     scoreHome.innerHTML = localStorage.getItem("sumHome")
     scoreGuest.innerHTML = localStorage.getItem("sumGuest")
-}
+})
